70b4519fee2513d130b6be4b6405ff715e272b6f
Bailey Van Wormer <bvan-wormer@unc.edu>
2025-05-27 12:44:54 +0200

    changed last few lines of app.py for hosting, may need to revert later

--- backend/app.py
+++ backend/app.py
@@ -190,5 +190,9 @@ def serve_react(path):
 
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(
+        host="0.0.0.0", 
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
 
