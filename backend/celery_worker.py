0c9e63911fb423623b4c6c6008260b45c9d592d1
Bailey Van Wormer <bvan-wormer@unc.edu>
2025-05-29 16:19:06 -0400

    increase celerey time to 5 mins before time-out for large exports

--- backend/celery_worker.py
+++ backend/celery_worker.py
@@ -11,4 +11,10 @@ celery_app = Celery(
     backend=redis_url
 )
 
+celery_app.conf.update(
+    task_time_limit=300,        # hard kill after 5 min
+    task_soft_time_limit=270,   # raises SoftTimeLimitExceeded at 4.5 min
+    timezone='UTC'
+)
+
 import tasks  # ensures tasks get registered
