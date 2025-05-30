3631013fa9775824e778828eb4a5132c4c39e40d
Bailey Van Wormer <bvan-wormer@unc.edu>
2025-05-30 09:43:57 -0400

    took out background color, will need to add logic for dark mode

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -2,6 +2,14 @@
 const Header = () => {
     return (
       <header className="site-header">
+        <div class="dropdown">
+            <button onclick="myFunction()" class="dropbtn">Dropdown</button>
+            <div id="myDropdown" class="dropdown-content">
+                <a href="#">Link 1</a>
+                <a href="#">Link 2</a>
+                <a href="#">Link 3</a>
+            </div>
+        </div>
         <h2>ExportMyMusic.com</h2>
       </header>
     );
